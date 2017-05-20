85e31f96914f634feb0d0816e7641f0cbaa884b6
tanglie1993 <tanglie23@163.com>
2017-05-20 20:45:04 +0800

    default arguments

---
+++ src/DefaultArguments.kt
@@ -0,0 +1,16 @@
+/**
+ * Created by Administrator on 2017/5/20 0020.
+ */
+fun foo(name: String, number: Int = 42, toUpperCase: Boolean = false) =
+        (if (toUpperCase) name.toUpperCase() else name) + number
+
+fun useFoo() = listOf(
+        foo("a"),
+        foo("b", number = 1),
+        foo("c", toUpperCase = true),
+        foo(name = "d", number = 2, toUpperCase = true)
+)
+
+fun main(args : Array<String>){
+    println(useFoo())
+}
