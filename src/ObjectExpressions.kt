656a157c996d04ae507888d4aa43a40cf7278e90
tanglie1993 <tanglie23@163.com>
2017-05-21 14:05:31 +0800

    object expressions

---
+++ src/ObjectExpressions.kt
@@ -0,0 +1,14 @@
+/**
+ * Created by Administrator on 2017/5/21 0021.
+ */
+import java.util.*
+
+fun getList(): List<Int> {
+    val arrayList = arrayListOf(1, 5, 2)
+    Collections.sort(arrayList, { o1, o2 -> if (o1 > o2) 1 else -1 })
+    return arrayList
+}
+
+fun main(args : Array<String>){
+    println(getList())
+}
