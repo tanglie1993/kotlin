3376e06c47922e4d93ef1156e09df7038ddd62c7
tanglie1993 <tanglie23@163.com>
2017-05-22 00:05:21 +0800

    filter map

---
+++ src/collections/FilterMap.kt
@@ -0,0 +1,10 @@
+package collections
+
+/**
+ * Created by Administrator on 2017/5/22 0022.
+ */
+// Return the set of cities the customers are from
+fun Shop.getCitiesCustomersAreFrom(): Set<City> = customers.map{it.city}.toSet()
+
+// Return a list of the customers who live in the given city
+fun Shop.getCustomersFrom(city: City): List<Customer> = customers.filter{it.city == city}
