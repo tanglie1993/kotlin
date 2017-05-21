0783b56b0e6d7c46ee5b2fa0fb851f958ca909ec
tanglie1993 <tanglie23@163.com>
2017-05-21 22:51:30 +0800

    range to

---
+++ src/conventions/RangeTo.kt
@@ -0,0 +1,12 @@
+package conventions
+
+/**
+ * Created by Administrator on 2017/5/21 0021.
+ */
+operator fun MyDate.rangeTo(other: MyDate) = DateRange(this, other)
+
+class DateRange(override val start: MyDate, override val endInclusive: MyDate): ClosedRange<MyDate>
+
+fun checkInRange(date: MyDate, first: MyDate, last: MyDate): Boolean {
+    return date in first..last
+}
