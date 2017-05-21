e8b85cdc57664d2dbca2953331011026272b4d70
tanglie1993 <tanglie23@163.com>
2017-05-21 23:58:39 +0800

    invoke

---
+++ src/conventions/invoke.kt
@@ -0,0 +1,15 @@
+package conventions
+
+/**
+ * Created by Administrator on 2017/5/21 0021.
+ */
+class Invokable {
+    var numberOfInvocations: Int = 0
+        private set
+    operator fun invoke(): Invokable {
+        numberOfInvocations++
+        return this
+    }
+}
+
+fun invokeTwice(invokable: Invokable) = invokable()()
