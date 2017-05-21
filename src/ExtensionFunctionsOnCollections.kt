c1b86d6ea13e58bfade9a134e69132caa756a165
tanglie1993 <tanglie23@163.com>
2017-05-21 14:21:38 +0800

    ExtensionFunctions

---
+++ src/ExtensionFunctionsOnCollections.kt
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2017/5/21 0021.
+ */
+
+fun getList(): List<Int> {
+    return arrayListOf(1, 5, 2).sortedDescending()//TODO("return the list sorted in descending order")
+}
+fun main(args : Array<String>){
+    println(getList())
+}
--- src/ObjectExpressions.kt
+++ src/ObjectExpressions.kt
@@ -5,7 +5,7 @@ import java.util.*
 
 fun getList(): List<Int> {
     val arrayList = arrayListOf(1, 5, 2)
-    Collections.sort(arrayList, { o1, o2 -> if (o1 > o2) 1 else -1 })
+    Collections.sort(arrayList, { o1, o2 -> if (o1 > o2) -1 else 1 })
     return arrayList
 }
 
