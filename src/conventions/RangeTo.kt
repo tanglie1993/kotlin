8f0fcb8ceb851558cba05ffc0dc59c14afba3b9b
tanglie1993 <tanglie23@163.com>
2017-05-29 13:11:22 +0800

    predicates

--- src/conventions/RangeTo.kt
+++ src/conventions/RangeTo.kt
@@ -1,16 +1,12 @@
 package conventions
 
 /**
- * Created by Administrator on 2017/5/21 0021.
+ * Created by Administrator on 2017/5/28 0028.
  */
 operator fun MyDate.rangeTo(other: MyDate) = DateRange(this, other)
 
-class DateRange(override val start: MyDate, override val endInclusive: MyDate): ClosedRange<MyDate> {
-    operator fun iterator(): Iterator<MyDate> {
+class DateRange(override val start: MyDate, override val endInclusive: MyDate): ClosedRange<MyDate>
 
-    }
-}
-
-private fun checkInRange(date: MyDate, first: MyDate, last: MyDate): Boolean {
+fun checkInRange(date: MyDate, first: MyDate, last: MyDate): Boolean {
     return date in first..last
 }
