8c3b000ce66fa2fdf5e42099f08d6e8d9bf93927
tanglie1993 <tanglie23@163.com>
2017-05-30 16:26:38 +0800

    lazy property

---
+++ src/properties/LazyProperty.kt
@@ -0,0 +1,15 @@
+package properties
+
+/**
+ * Created by Administrator on 2017/5/30 0030.
+ */
+class LazyProperty(val initializer: () -> Int) {
+    var test:Int? = null
+    val lazy: Int
+        get() {
+            if(test == null){
+                test = initializer()
+            }
+            return test as Int
+        }
+}
